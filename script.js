a060ceae96c1f3a6a8dfd3ef7168ed1cb8210620
Aldin <alhuric89@gmail.com>
2023-03-29 07:08:47 +0200

    add container switching functionality

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -30,13 +31,45 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-
-const section1 = document.querySelector('#section--1');
-
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
 
   //scrolling
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//page navigation
+//1add event listener to parent
+//2 determine where event fired
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log(e.target);
+  e.preventDefault();
+  //matching
+  if (e.target.classList.contains('nav__link')) {
+    console.log('link');
+
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+//tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+  //guard clause
+  if (!clicked) return;
+  tabs.forEach(e => e.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+  tabsContent.forEach(e => e.classList.remove('operations__content--active'));
+  //activating content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
